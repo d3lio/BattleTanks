19b886be88b2b329ad595625dfee6df3d4b8022f
Nikola Stoyanov <ns.barzakov@gmail.com>
2016-06-13 13:37:26 +0300

    Added documentation.

--- engine/src/core/input/mask.rs
+++ engine/src/core/input/mask.rs
@@ -34,7 +34,7 @@ impl KeyMask {
 
     /// Set the bits associated with a range of keys.
     ///
-    /// `range` is inclusive, that is a range `Key::A .. Key::Z` will include `Key::Z`
+    /// `range` is inclusive, that is a range `Key::A .. Key::Z` will include `Key::A` and `Key::Z`.
     pub fn set_range(&mut self, range: Range<glfw::Key>, val: bool) {
         for key in range.start as usize .. range.end as usize + 1 {
             let index = GLFW_KEY_MAP[key];
@@ -108,7 +108,7 @@ const GLFW_KEY_MAP: [isize; glfw::ffi::KEY_LAST as usize + 1] = [
      64,  65,  66,  67,  68,  -1,  -1,  -1,  -1,  -1,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,
      79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  -1,  -1,  -1,  -1,  -1,
      94,  95,  96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110,  -1,  -1,  -1,
-     111, 112, 113, 114, 115, 116, 117, 118, 119,
+    111, 112, 113, 114, 115, 116, 117, 118, 119,
 ];
 
 /// Number of entries in the glfw enum
--- engine/src/core/input/mod.rs
+++ engine/src/core/input/mod.rs
@@ -1,8 +1,22 @@
-//! Input managment system
-
-// A system to manage the distribution of input events. Consists of a single manager class and multiple listeners.
-// The manager is fed all input events from the system. It also keeps track of the listeners using a stack-like structure. Whenever a listener receives focus it is moved to the top of the stack.
-// Events are send to the listeners in a capturing manner, starting at the top of the stack. I.e. only one listener can receive an event and that is the listener closest to the top of the stack that has the corresponding callback set (unless passtrough ...).
+//! Input management system
+//!
+//! A system to manage the distribution of input events.
+//! Consists of a manager class and multiple listener classes for different events.
+//!
+//! The listener classes represent a single object that can receive input events.
+//! Callback functions can be subscribed for the various events.
+//! Listeners have the concept of "focus". When the user "selects" the object the listener
+//! is said to receive input focus. When the user "deselects" it the listener loses input
+//! focus. The listener can only receive events when it has focus.
+//!
+//! The manager is fed all input events from the system to distribute them among the listeners.
+//! It also keeps s stack of all listeners who are currently under focus, with the ones who most
+//! recently received focus being on the top of the stack.
+//!
+//! Events are send to the listeners in a capturing manner, starting at the top of the stack.
+//! The event is received by the listener closest to the top of the stack who has the corresponding
+//! callback function set. If that listener has the `passtrough` option set then that event is also
+//! passed to the listeners bellow him.
 
 extern crate glfw;
 
@@ -14,6 +28,13 @@ use std::rc::{Rc, Weak};
 
 pub use self::mask::KeyMask;
 
+// TODO: keep track of all key pressed events received and fire the
+// corresponding key released events upon losing focus.
+
+/// Listener for keyboard input events.
+///
+/// Listens for individual key strokes as determined by their keycodes and is thus independent
+/// of the current keyboard layout the user has selected.
 pub struct KeyListener {
     subscribers: Vec<(KeyMask, Rc<Fn(glfw::Key, glfw::Scancode, glfw::Action)>, bool)>,
     manager: ManagerWeak,
@@ -24,11 +45,19 @@ struct _Manager {
     window: *const glfw::Window,
 }
 
+/// Input event manager.
+///
+/// The manager should be fed all input events from the system event queue using one
+/// of the `emit_*` methods and it will distribute those events to all listeners who
+/// are currently on focus.
+///
+/// Usually only a single instance of this class per game window should be created.
 pub struct Manager (Rc<RefCell<_Manager>>);
+
 struct ManagerWeak (Option<Weak<RefCell<_Manager>>>);
 
-
 impl KeyListener {
+    /// Create a new `KeyListener`.
     pub fn new() -> KeyListener {
         KeyListener {
             subscribers: Vec::new(),
@@ -36,12 +65,31 @@ impl KeyListener {
         }
     }
 
+    /// Set a callback for a set of keys.
+    ///
+    /// `keys` is the set of keys for who the callback will be triggered.
+    ///
+    /// `callback` is the callback function. It receives the keycode, system specific scan code
+    /// and the action - pressed, released or repeat.
+    ///
+    /// `passtrough` should be set to `true` if you want other listeners to receive the same event.
+    ///
     pub fn on<F> (&mut self, keys: KeyMask, callback: Rc<F>, passtrough: bool) where
         F: Fn(glfw::Key, glfw::Scancode, glfw::Action) + 'static
     {
         self.subscribers.push((keys, callback, passtrough));
     }
 
+    /// Get the state of the key since the last polling of input events.
+    ///
+    /// This function follows the same rules as the event callbacks, i.e. listener
+    /// has to have focus and a callback function for `key` must have been set.
+    ///
+    /// The state of `key` will be returned only if a callback function for `key` from
+    /// this listener would have been called. In that case `true` is returned if the key
+    /// is pressed and `false` otherwise.
+    ///
+    /// In all other cases `false` is returned.
     pub fn key_pressed(&self, key: glfw::Key) -> bool {
         match self.manager.upgrade() {
             Some(mgr) => mgr.key_unbuffered(self, key),
@@ -69,10 +117,16 @@ impl _Manager {
 }
 
 impl Manager {
+    /// Create a new manager.
+    ///
+    /// `window` is a handle to the window object that will be used for polling buffered input.
     pub fn new(window: &glfw::Window) -> Manager {
         Manager(wrap!(_Manager::new(window)))
     }
 
+    /// Notify the manager that a listener has gained input focus.
+    ///
+    /// This puts the listener on the top of the stack.
     pub fn gain_key_focus(&self, focus: &mut KeyListener) {
         if let Some(mgr) = focus.manager.upgrade() {
             mgr.lose_key_focus(focus);
@@ -82,13 +136,30 @@ impl Manager {
         focus.manager = ManagerWeak(Some(Rc::downgrade(&self.0)));
     }
 
+    /// Notify the manager that a listener has gained input focus.
     pub fn lose_key_focus(&self, focus: &mut KeyListener) {
         let focus_ptr = focus as *mut _;
         self.0.borrow_mut().key_listeners.retain(|&lptr| lptr != focus_ptr);
         focus.manager = ManagerWeak(None);
     }
 
-    pub fn key_unbuffered(&self, listener: &KeyListener, key: glfw::Key) -> bool {
+    /// Feed the manager.
+    pub fn emit_key(&self, key: glfw::Key, scancode: glfw::Scancode, action: glfw::Action) {
+        unsafe {
+            for &listener in self.0.borrow().key_listeners.iter().rev() {
+                for &(ref mask, ref callback, passtrough) in &(*listener).subscribers {
+                    if mask.check(key) {
+                        callback(key, scancode, action);
+                        if !passtrough {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn key_unbuffered(&self, listener: &KeyListener, key: glfw::Key) -> bool {
         unsafe {
             let mgr = self.0.borrow();
 
@@ -111,21 +182,6 @@ impl Manager {
                 }
             }
             return false;
-        }
-    }
-
-    pub fn emit_key(&self, key: glfw::Key, scancode: glfw::Scancode, action: glfw::Action) {
-        unsafe {
-            for &listener in self.0.borrow().key_listeners.iter().rev() {
-                for &(ref mask, ref callback, passtrough) in &(*listener).subscribers {
-                    if mask.check(key) {
-                        callback(key, scancode, action);
-                        if !passtrough {
-                            return;
-                        }
-                    }
-                }
-            }
         }
     }
 }
